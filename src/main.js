c27223f7bc2167a84cb313d9c466ca139a813ded
milan102 <hi.milan102@gmail.com>
2018-12-08 01:01:04 -0800

    Syntax update

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    win = null;
   });
 
   require('./menu.js');
@@ -57,7 +57,7 @@ app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 });
 
@@ -65,6 +65,6 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
 });
